593882e77056b705c62ae3f32457cd2e7c1af346
Dabai1025 <65448593+Dabai1025@users.noreply.github.com>
2022-07-04 23:51:58 +0800

    移植上周目部分物品和方块 (#142)

---
+++ 20220704/kubejs/startup_scripts/custom_items.js
@@ -0,0 +1,28 @@
+// priority: 0
+
+onEvent('item.registry', event => {
+	const addIngot = ['nickel', 'copper', 'tin', 'aluminum', 'lead', 'silver', 'uranium', 'iridium', 'platinum']
+	for (i = 0; i < addIngot.length; i++) {
+		event.create(addIngot[i] + '_ingot')
+	}
+})
+
+onEvent('item.registry', event => {
+	const addEpicItem = ['star', 'primogem', 'dragon_catalyst', 'intertwined_fate', 'acquaint_fate', "clay_furnace"]
+	for (i = 0; i < addEpicItem.length; i++) {
+		event.create(addEpicItem[i])
+	}
+})
+
+onEvent('item.registry', event => {
+	event.create('paimon').food((food) => {
+		food.hunger(2.5).saturation(1.0).effect('minecraft:saturation', 1, 2, 1)
+	})
+})
+
+onEvent('block.registry', event => {
+	const addBlock = ["magic_log", "skull_block", "magic_stone"]
+	for (i = 0; i < addBlock.length; i++) {
+		event.create(addBlock[i])
+	}
+})
