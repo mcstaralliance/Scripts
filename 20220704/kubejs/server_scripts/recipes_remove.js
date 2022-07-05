7f521bbeb6b589bed6e1fbdcf3c75fdddfc5f4fd
Dabai1025 <65448593+Dabai1025@users.noreply.github.com>
2022-07-05 17:30:14 +0800

    删除物品配方

--- 20220704/kubejs/server_scripts/recipes_remove.js
+++ 20220704/kubejs/server_scripts/recipes_remove.js
@@ -3,7 +3,8 @@
 // 注意: 可以删除任何处理配方
 onEvent('recipes', event => {
     const remove = [
-        'draconicevolution:reactor_core'
+        'draconicevolution:reactor_core',
+        'botania:apothecary_default'
     ]
     for (i = 0; i < remove.length; i++){
         event.remove({output:remove[i]})
