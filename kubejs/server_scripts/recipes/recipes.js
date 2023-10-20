9ea474cdfc376e4597ba140da51dec867e1d1e01
PM-pinou <2504420230@qq.com>
2023-10-20 21:20:54 +0800

    review

--- kubejs/server_scripts/recipes/recipes.js
+++ kubejs/server_scripts/recipes/recipes.js
@@ -4,17 +4,41 @@ const addFullShapedRecipe = global.addFullShaped;
 const addCrossRecipe = global.addCross
 const addHalfFullShapelessRecipe = global.addHalfFullShapeless
 
+
+// 各形状配方形状
+/* const patterns = {
+    crossWithCore: [
+        'BAB', 
+        'ACA',
+        'BAB'
+    ],
+    surroundWithCore: [
+        'AAA',
+        'ABA',
+        'AAA'
+    ],
+    fullShaped: [
+        'AAA',
+        'AAA',
+        'AAA'
+    ],
+    cross: [
+        'ABA',
+        'BAB',
+        'ABA'
+    ]
+}; */
+
+
+
 onEvent('recipes', event => {
-    // ABA BCB ABA
+    // 传入参数是首先是 output，然后是 core(如果有core的话)，然后是 surroundA、surroundB(如果有的话)
     addCrossWithCoreRecipe(event, 'minecraft:blast_furnace', 'minecraft:furnace', 'minecraft:iron_ingot', 'minecraft:bricks'); // 高炉
 
-    // AAA ABA AAA
     addSurroundWithCoreRecipe(event, 'minecraft:furnace', '#forge:cobblestone', 'minecraft:clay'); // 熔炉
 
-    // AAA AAA AAA
     addFullShapedRecipe(event, 'minecraft:snow_block', 'minecraft:snowball'); // 雪块
 
-    // ABA BAB ABA
     addCrossRecipe(event, '12x minecraft:coarse_dirt', '#forge:dirt', '#forge:gravel'); // 砂土
 
 });
