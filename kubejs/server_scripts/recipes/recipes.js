9a0f4a594256df9bb9fd77a2fd3621a6eecc9eb4
Funny Baymax <65448593+Dabai1025@users.noreply.github.com>
2023-11-04 18:45:33 +0800

    Update recipes.js

--- kubejs/server_scripts/recipes/recipes.js
+++ kubejs/server_scripts/recipes/recipes.js
@@ -1,27 +1,3 @@
-// 各形状配方形状
-/* const patterns = {
-    crossWithCore: [
-        'BAB', 
-        'ACA',
-        'BAB'
-    ],
-    surroundWithCore: [
-        'AAA',
-        'ABA',
-        'AAA'
-    ],
-    fullShaped: [
-        'AAA',
-        'AAA',
-        'AAA'
-    ],
-    cross: [
-        'ABA',
-        'BAB',
-        'ABA'
-    ]
-}; */
-
 onEvent('recipes', (event) => {
     const recipesShaped = [
         {
@@ -50,21 +26,3 @@ onEvent('recipes', (event) => {
         event.shaped(recipe.output, recipe.pattern, recipe.key).id(recipe.id)
     })
 })
-
-
-
-// onEvent('recipes', event => {
-//     // 传入参数是首先是 output，然后是 core(如果有core的话)，然后是 surroundA、surroundB(如果有的话)
-//     addCrossWithCoreRecipe(event, 'minecraft:blast_furnace', 'minecraft:furnace', 'minecraft:iron_ingot', 'minecraft:bricks'); // 高炉
-//     addCrossWithCoreRecipe(event, '6x immersiveengineering:blastbrick', 'minecraft:blast_furnace', 'minecraft:magma_block', 'minecraft:red_nether_bricks'); // 高炉砖
-//     addCrossWithCoreRecipe(event, 'thermal:machine_frame', '#forge:gears/invar', 'glassential:glass_dark', 'thermal:signalum_ingot'); // 机器框架
-
-//     addSurroundWithCoreRecipe(event, 'minecraft:furnace', '#forge:cobblestone', 'minecraft:clay'); // 熔炉
-
-//     addFullShapedRecipe(event, 'minecraft:snow_block', 'minecraft:snowball'); // 雪块
-
-//     addCrossRecipe(event, '12x minecraft:coarse_dirt', 'minecraft:dirt', '#forge:gravel'); // 砂土
-
-//     event.shapeless('immersiveengineering:blastbrick_reinforced', ['immersiveengineering:blastbrick', 'botania:blaze_block', '#forge:plates/steel']); // 强化高炉砖
-// });
-// 
