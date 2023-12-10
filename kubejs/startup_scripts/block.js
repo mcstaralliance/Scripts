02e5007dd50f27b6ffba8b72ab7dda452fd0ba23
x6eull <x6eull@gmail.com>
2023-12-10 21:40:49 +0800

    Simplify startup_scripts

--- kubejs/startup_scripts/block.js
+++
@@ -1,7 +0,0 @@
-onEvent('block.registry', event => {
-    var array = new Array(55);
-    for (count = 1; count <= 55; count++) {
-        array[count] = count
-        event.create('block_' + count).material('metal').hardness(1).lightLevel(1.0)
-    }
-})
