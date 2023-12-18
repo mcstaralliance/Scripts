3a6fe5c20b7575461bc8987310ca513ba6d5c3a8
x6eull <41849785+x6eull@users.noreply.github.com>
2023-12-18 18:46:18 +0800

    Simplify startup_scripts (#226)

--- kubejs/startup_scripts/item.js
+++
@@ -1,7 +0,0 @@
-onEvent('item.registry', event => {
-    var array = new Array(3200);
-    for (count = 1; count <= 3200; count++) {
-        array[count] = count
-        event.create('item_' + count)
-    }
-})
