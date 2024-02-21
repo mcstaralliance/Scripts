9a0ac08998291c768ddcd7dbf05f91e60f0eb184
LDS_XiaoYe <xiaoyecreate@outlook.com>
2024-02-21 23:18:18 +0800

    :sparkles: Sync custom items

---
+++ kubejs/server_scripts/script.js
@@ -0,0 +1,8 @@
+// priority: 0
+
+settings.logAddedRecipes = true
+settings.logRemovedRecipes = true
+settings.logSkippedRecipes = false
+settings.logErroringRecipes = true
+
+console.info('Hello, World! (You will see this line every time server resources reload)')
