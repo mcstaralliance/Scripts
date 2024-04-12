caca06b0359e3a6a11bae821d55d88c5baf0664a
Funny Baymax <1025765097@qq.com>
2024-04-12 15:50:52 +0800

    update

--- kubejs/server_scripts/thermal/smelt.js
+++
@@ -1,3 +0,0 @@
-onEvent('recipes', event => {
-    event.recipes.thermal.smelter('4x integrateddynamics:cable', ['integrateddynamics:crystalized_menril_block', '#forge:dusts/redstone']).energy(25000)
-})
