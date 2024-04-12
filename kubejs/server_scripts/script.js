7bd11f9e01688c900e34fe43d8090de113949c8e
Funny Baymax <1025765097@qq.com>
2024-04-12 15:33:59 +0800

    ✨update

--- kubejs/server_scripts/script.js
+++
@@ -1,8 +0,0 @@
-// priority: 0
-
-settings.logAddedRecipes = true
-settings.logRemovedRecipes = true
-settings.logSkippedRecipes = false
-settings.logErroringRecipes = true
-
-console.info('Hello, World! (You will see this line every time server resources reload)')
