f786a6110d98b722b403fdbd7cf400d10b38c9f4
Funny Baymax <65448593+Dabai1025@users.noreply.github.com>
2024-05-01 19:06:22 +0800

    useless code

--- kubejs/startup_scripts/registry.js
+++ kubejs/startup_scripts/registry.js
@@ -22,7 +22,7 @@ onEvent('block.registry', event => {
     event.create('paimon_miner_pro').unbreakable().noDrops()
     event.create('paimon_miner_lite').unbreakable().noDrops()
     event.create('trophy').unbreakable()
-    event.create('custom_chest').rarity(RARITY_EPIC)
+    event.create('custom_chest')
 })
 
 onEvent('item.registry', event => {
