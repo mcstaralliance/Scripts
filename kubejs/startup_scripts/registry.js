b1c4d4197d51fb84fc3aac5ccb1f80525539ff8e
LDS_XiaoYe <xiaoyecreate@outlook.com>
2025-01-21 01:48:05 +0800

    添加硬币

--- kubejs/startup_scripts/registry.js
+++ kubejs/startup_scripts/registry.js
@@ -14,3 +14,11 @@ StartupEvents.registry('block', event => {
     event.create("paimoncyclone_miner_base");
     event.create("paimoncyclone_miner_pro");
 })
+
+// 注册硬币
+StartupEvents.registry('item', event => {
+    event.create("starcraft_commoncoin").rarity("common").displayName("铜币").tooltip(Component.gray("面值: 50"));
+    event.create("starcraft_advancedcoin").rarity("uncommon").displayName("银币").tooltip(Component.gray("面值: 600"));
+    event.create("starcraft_ultracoin").rarity("rare").displayName("金币").tooltip(Component.gray("面值: 1200"));
+})
+
