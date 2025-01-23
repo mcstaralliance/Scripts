98984529245e16b6e2d636e71d52d1e29f93ea75
Funny Baymax <1025765097@qq.com>
2025-01-24 00:09:10 +0800

    Update recipes

---
+++ kubejs/server_scripts/recipes.js
@@ -0,0 +1,48 @@
+ServerEvents.recipes(event => {
+    event.shaped('starcraft:energy_input', ["AAA","ABA","AAA"],
+        {
+            A: 'minecraft:iron_ingot',
+            B: 'minecraft:redstone_block'
+        }
+    );
+
+    event.shaped('starcraft:item_input', ["ABA","ACA","AAA"],
+        {
+            A: 'minecraft:iron_ingot',
+            B: 'minecraft:dispenser',
+            C: 'minecraft:hopper'
+        }
+    );
+
+    event.shaped('starcraft:item_output', ["AAA","ABA","ACA"],
+        {
+            A: 'minecraft:iron_ingot',
+            B: 'minecraft:dispenser',
+            C: 'minecraft:hopper'
+        }
+    );
+
+    event.shapeless('starcraft:item_input', ['starcraft:item_input']);
+    event.shapeless('starcraft:item_output', ['starcraft:item_output']);
+
+    event.shaped('starcraft:mana_input', ["ABA","ACA","AAA"],
+        {
+            A: 'minecraft:lapis_block',
+            B: 'botania:elven_spreader',
+            C: 'botania:mana_pylon'
+        }
+    );
+
+    event.shaped('starcraft:mana_output', ["AAA","ABA","ACA"],
+        {
+            A: 'minecraft:lapis_block',
+            B: 'botania:elven_spreader',
+            C: 'botania:mana_pylon'
+        }
+    );
+
+    event.shapeless('starcraft:mana_input', ['starcraft:mana_input']);
+    event.shapeless('starcraft:mana_output', ['starcraft:mana_output']);
+    event.shapeless('createaddition:electric_motor', ['starcraft:stress_input']);
+    event.shapeless('starcraft:stress_input', ['createaddition:electric_motor']);
+})
