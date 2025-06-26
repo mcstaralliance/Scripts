417bcabafbf01e1d7a0f508331c25edd2cfa2d46
qianbing <2247853786@qq.com>
2025-06-26 20:47:14 +0800

    新增了无暇赛特斯母岩的合成，添加对于紫晶傀儡的合成，让维度矿井能产出更多的矿

---
+++ kubejs/server_scripts/Ars Nouveau.js
@@ -0,0 +1,19 @@
+ServerEvents.recipes(event => {
+event.recipes.ars_nouveau.enchanting_apparatus(
+        [
+            "minecraft:amethyst_block",
+            "minecraft:amethyst_block",
+            "minecraft:amethyst_block",
+            "minecraft:amethyst_block",
+            "ars_nouveau:ritual_awakening",
+            "ars_nouveau:ritual_awakening",
+            "ars_nouveau:ritual_awakening",
+            "ars_nouveau:ritual_awakening",
+           
+        ], 
+    "ars_nouveau:ritual_brazier", 
+    "ars_nouveau:amethyst_golem_charm", 
+    2000, 
+     
+);
+})
