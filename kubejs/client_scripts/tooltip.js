298d11db0c61795a26e1a2d5e91073d604a7be4e
Dabai1025 <1025765097@qq.com>
2025-07-03 17:19:46 +0800

    ban items

--- kubejs/client_scripts/tooltip.js
+++ kubejs/client_scripts/tooltip.js
@@ -4,7 +4,23 @@ const itemToBan = [
     'create:schematicannon',
     'torcherino:torcherino',
     'torcherino:compressed_torcherino',
-    'torcherino:double_compressed_torcherino'
+    'torcherino:double_compressed_torcherino',
+    'ars_nouveau:glyph_phantom_block',
+    'ars_nouveau:glyph_place_block',
+    'ars_nouveau:glyph_wall',
+    'ars_nouveau:glyph_intangible',
+    'mekanism:robit',
+    'mekanism:upgrade_anchor',
+    'pneumaticcraft:chunkloader_upgrade',
+    'evilcraft:lightning_bomb',
+    'industrialforegoing:enchantment_applicator',
+    'ae2:matter_cannon',
+    'twilightforest:uncrafting_table',
+    'ifeu:time_controller',
+    'ifeu:rule_controller',
+    'create:mechanical_plough',
+    'create:mechanical_drill',
+    'create:mechanical_roller'
 ]
 
 ItemEvents.tooltip(tooltip => {
@@ -26,4 +42,6 @@ JEIEvents.information(event => {
     event.addItem('xycraft_world:kivi', ['按住 w 进行思索'])
     event.addItem('minecraft:beehive', ['如果橡树树苗、白桦树苗或樱花树苗水平2格范围内存在花、红树胎生苗、盛开的杜鹃花丛或盛开的杜鹃树叶，则该树苗长成的树会有 5% 的概率悬挂着蜂巢。'])
     event.addItem('ae2:mysterious_cube', ['按住 w 进行思索'])
+    event.addItem('pneumaticcraft:ingot_iron_compressed', ['可以使用迷你 TNT 进行爆炸获取'])
+    event.addItem('pneumaticcraft:compressed_iron_block', ['可以使用迷你 TNT 进行爆炸获取'])
 })
