6a7cdf3dae57b6c7292c6cefc9afdeed690f5414
qianbing233 <59565986+qianbing233@users.noreply.github.com>
2026-01-27 01:34:11 +0800

    魔改第五弹 (#299)

--- kubejs/server_scripts/scripts/immersiveengineering/arc_furnace.js
+++ kubejs/server_scripts/scripts/immersiveengineering/arc_furnace.js
@@ -5,4 +5,10 @@ ServerEvents.recipes(event => {
         ['ftbmaterials:coal_coke_dust'], 
         'ftbmaterials:emerald_dust'
     )
+
+    event.recipes.immersiveengineering.arc_furnace(
+        ['draconicevolution:draconium_ingot'],
+        'draconicevolution:draconium_dust',
+        ['iceandfire:dragonsteel_fire_ingot','iceandfire:dragonsteel_ice_ingot','iceandfire:dragonsteel_lightning_ingot']
+    )
 })
