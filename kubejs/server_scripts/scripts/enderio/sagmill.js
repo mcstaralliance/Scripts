2ea31a677d94b99d34fa200bc38c4154b4a5b550
qianbing233 <59565986+qianbing233@users.noreply.github.com>
2026-02-04 23:54:55 +0800

    修复硫粉合成 (#307)

--- kubejs/server_scripts/scripts/enderio/sagmill.js
+++ kubejs/server_scripts/scripts/enderio/sagmill.js
@@ -1,3 +1,9 @@
 ServerEvents.recipes(event => {
     event.recipes.enderio.sag_milling(['32x fluxnetworks:flux_core'], 'mekanism:quantum_entangloporter')
+    
+    event.recipes.enderio.sag_milling(['ftbmaterials:sulfur_dust'], 'ftbmaterials:sulfur')
+
+    event.recipes.enderio.sag_milling(['4x ftbmaterials:sulfur_dust'], 'ftbmaterials:sulfur_stone_ore')
+
+    event.recipes.enderio.sag_milling(['ftbmaterials:salt_dust'],'ftbmaterials:salt')
 })
--- kubejs/server_scripts/scripts/mekanism/crushing.js
+++ kubejs/server_scripts/scripts/mekanism/crushing.js
@@ -7,4 +7,11 @@ ServerEvents.recipes(event => {
 
     //钙粉
     event.recipes.mekanism.crushing('starcraft:dust_calcium','minecraft:bone_meal')
+
+    //硫粉
+    event.recipes.mekanism.crushing('ftbmaterials:sulfur_dust','ftbmaterials:sulfur')
+    event.recipes.mekanism.crushing('4x ftbmaterials:sulfur_dust','ftbmaterials:sulfur_stone_ore')
+
+    //盐粉
+    event.recipes.mekanism.crushing('ftbmaterials:salt_dust','ftbmaterials:salt')
 })
