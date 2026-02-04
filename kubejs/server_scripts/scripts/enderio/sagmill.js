ebd5a0298c6f461ddc6b8c1cc769d47692696f08
qianbing <2247853786@qq.com>
2026-02-04 11:39:27 +0800

    添加硫矿获得方式

--- kubejs/server_scripts/scripts/enderio/sagmill.js
+++ kubejs/server_scripts/scripts/enderio/sagmill.js
@@ -2,4 +2,6 @@ ServerEvents.recipes(event => {
     event.recipes.enderio.sag_milling(['32x fluxnetworks:flux_core'], 'mekanism:quantum_entangloporter')
     
     event.recipes.enderio.sag_milling(['ftbmaterials:sulfur_dust'], 'ftbmaterials:sulfur')
+
+    event.recipes.enderio.sag_milling(['4x ftbmaterials:sulfur_dust'], 'ftbmaterials:sulfur_stone_ore')
 })
--- kubejs/server_scripts/scripts/mekanism/crushing.js
+++ kubejs/server_scripts/scripts/mekanism/crushing.js
@@ -10,4 +10,5 @@ ServerEvents.recipes(event => {
 
     //硫粉
     event.recipes.mekanism.crushing('ftbmaterials:sulfur_dust','ftbmaterials:sulfur')
+    event.recipes.mekanism.crushing('4x ftbmaterials:sulfur_dust','ftbmaterials:sulfur_stone_ore')
 })
