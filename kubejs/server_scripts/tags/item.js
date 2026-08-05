81d540f6edc0bda3538a99b8693db41d3052d599
qianbing <2247853786@qq.com>
2026-08-05 22:57:30 +0800

    临时修复钛锭问题

--- kubejs/server_scripts/tags/item.js
+++ kubejs/server_scripts/tags/item.js
@@ -9,5 +9,6 @@ ServerEvents.tags("item",(event) =>{
     event.add('minecraft:redstone_ores',['ftbmaterials:redstone_cluster']),
     event.add('pneumaticcraft:plastic_sheets',['industrialforegoing:plastic']);
     event.remove('c:ores/iron',['ad_astra:mars_iron_ore']);
-    event.remove('minecraft:iron_ores',['ad_astra:mars_iron_ore'])
+    event.remove('minecraft:iron_ores',['ad_astra:mars_iron_ore']);
+    event.remove('c:plates/obsidian',['create:sturdy_sheet'])
 })
