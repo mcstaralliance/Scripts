56592a99c92305e37433c7a2924cebe3c138dee3
qianbing <2247853786@qq.com>
2026-08-05 19:12:18 +0800

    修复火星钢以及集气问题

--- kubejs/server_scripts/tags/block.js
+++ kubejs/server_scripts/tags/block.js
@@ -1,3 +1,5 @@
 ServerEvents.tags("block",(event) =>{
     event.add('createdieselgenerators:oil_deposit',["minecraft:air"]);
+    event.remove('c:ores/iron',['ad_astra:mars_iron_ore'])
+    event.remove('minecraft:iron_ores',['ad_astra:mars_iron_ore'])
 })
