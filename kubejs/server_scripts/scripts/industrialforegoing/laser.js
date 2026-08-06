4f3cd2bb1c5694b7a0551c22064773d3982d78d7
qianbing233 <59565986+qianbing233@users.noreply.github.com>
2026-08-07 01:14:24 +0800

    修复火星灰的问题 (#331)

--- kubejs/server_scripts/scripts/industrialforegoing/laser.js
+++ kubejs/server_scripts/scripts/industrialforegoing/laser.js
@@ -152,6 +152,31 @@ ServerEvents.recipes(event=> {
   event.custom({
     "type": "industrialforegoing:laser_drill_ore",
     "catalyst": {
+      "item": "industrialforegoing:black_laser_lens"
+    },
+    "output": {
+      "count": 2,
+      "item": 'northstar:volcanic_ash'
+    },
+    "rarity": [
+      {
+        "biome_filter": {
+          "blacklist": [],
+          "whitelist": ['ad_astra:is_mars']
+        },
+        "depth_max": 255,
+        "depth_min": 0,
+        "dimension_filter": {
+          "blacklist": [],
+          "whitelist": []
+        },
+        "weight": 28
+      },
+    ]
+  })
+  event.custom({
+    "type": "industrialforegoing:laser_drill_ore",
+    "catalyst": {
       "item": "industrialforegoing:red_laser_lens"
     },
     "output": {
